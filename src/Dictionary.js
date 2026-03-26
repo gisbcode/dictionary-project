97426608f56edf752355cab76e9c6e9e9b72fe23
Gigi <creative.giovannasb@gmail.com>
2026-03-26 21:54:45 +0000

    Added keyword function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,23 @@
-
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
 
-    function Search(event) {
-        event.preventdefault();
-        alert("Searching");
-    }
+  function Search(event) {
+    event.preventdefault();
+    alert(`Searching for ${keyword} definition`);
+  }
 
+  function keywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-    return (
-        <div className="Dictionary">
-            <form onSubmit={Search}>
-                <input type="search" autoFocus={true} />
-            </form>
-        </div>
-    );
-
-
+  return (
+    <div className="Dictionary">
+      <form onSubmit={Search}>
+        <input type="search" autoFocus={true} onChange={keywordChange} />
+      </form>
+    </div>
+  );
 }
