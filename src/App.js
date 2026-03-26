1938c84f412b55d0db2c6416a0d9d267702345fb
Gigi <creative.giovannasb@gmail.com>
2026-03-26 14:26:05 +0000

    Dictionary file created

--- src/App.js
+++ src/App.js
@@ -4,9 +4,37 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">React Dictionary Project</header>
+      <h1>React Dictionary Project</h1>
+      <Dictionary />
+      <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/gisbcode"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Giovanna Bellon
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/gisbcode/reactfinalapp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://reactweatherappgio.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
     </div>
   );
 }
 
 export default App;
+  
