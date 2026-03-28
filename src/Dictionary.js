c7894aeb0967851f51dfc09c860b723900b34798
Gigi <creative.giovannasb@gmail.com>
2026-03-28 23:08:29 +0000

    Add apiURL and function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,10 +5,17 @@ import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleHesponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
   }
+
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+  axios.get(apiUrl).then(handleResponse);
 
   function keywordChange(event) {
     setKeyword(event.target.value);
