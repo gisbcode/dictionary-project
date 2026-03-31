a84ba0d3437139d1a026cad5c114fd4fd52316a0
Gigi <creative.giovannasb@gmail.com>
2026-03-31 11:14:18 +0100

    fixing results

--- src/Results.js
+++ src/Results.js
@@ -2,18 +2,19 @@ import React from "react";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
-  if (!props.results || !props.results.meanings) {
+  if (!props.results) {
     return null;
   }
-
   return (
     <div className="Results">
       <h2>{props.results.word}</h2>
 
-      {props.results.phonetic && <h3>{props.results.phonetic}</h3>}
-
       {props.results.meanings.map(function (meaning, index) {
-        return <Meaning meaning={meaning} key={index} />;
+        return (
+        <div key={index}>
+        <Meaning meaning={meaning} />
+        </div>
+        );
       })}
     </div>
   );
