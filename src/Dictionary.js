0c8cb690a915d51684c30a171a330575773433dc
Gigi <creative.giovannasb@gmail.com>
2026-04-01 17:10:28 +0100

    fixing name

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
@@ -6,51 +6,46 @@ import "./Dictionary.css";
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
-  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search() {
+  const load = useCallback(() => {
     let apiKey = "39a3014fd34afe90bc14c4tc7oed280d";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
-  }
+  }, [keyword]);
+
+  
+  useEffect(() => {
+    load();
+  }, [load]);
 
   function handleSubmit(event) {
     event.preventDefault();
-    search();
+    load();
   }
 
   function updateKeyword(event) {
     setKeyword(event.target.value);
   }
 
-  function load() {
-    setLoaded(true);
-    search();
-  }
-
-  useEffect(() => {
-    if (!loaded) {
-      load();
-    }
-  }, [loaded]);
-
   return (
     <div className="Dictionary">
       <section className="search-box">
         <h1>What word do you want to look up?</h1>
+
         <form onSubmit={handleSubmit}>
           <input
             type="search"
             onChange={updateKeyword}
-            defaultValue={props.defaultKeyword}
+            value={keyword}
             placeholder="Type a word..."
-            autoFocus={true}
+            autoFocus
           />
         </form>
+
         <small>i.e. paris, wine, yoga, coding</small>
       </section>
 
