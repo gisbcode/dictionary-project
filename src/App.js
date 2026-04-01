ab92ab64542fc60aa9114f092b28bd6cec53c724
Gigi <creative.giovannasb@gmail.com>
2026-04-01 14:11:44 +0100

    add correct github link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         </a>{" "}
         and is open-sourced on{" "}
         <a
-          href="https://github.com/gisbcode/reactfinalapp"
+          href="https://github.com/gisbcode/dictionary-project"
           target="_blank"
           rel="noreferrer"
         >
