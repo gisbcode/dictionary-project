6c3b8d12da5c3dce45f0d4fc3575236a69f5256e
Gigi <creative.giovannasb@gmail.com>
2026-04-07 17:46:30 +0100

    Added phonetic sound

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -6,5 +6,11 @@ export default function Phonetic(props) {
     return null;
   }
 
-  return <div className="Phonetic">{props.phonetic}</div>;
+  return <div className="Phonetic">
+    <a href={props.phonetic.audio} target="_blank">
+      Listen
+    </a>
+    <br />
+    {props.phonetic.text}
+    </div>;
 }
