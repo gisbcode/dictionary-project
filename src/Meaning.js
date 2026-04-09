a71a5ad29d8def86f7112bc5e4aa28a1899f2cbc
Gigi <creative.giovannasb@gmail.com>
2026-04-09 16:32:33 +0100

    Fixing some css

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,29 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
+  if (!props.meaning || !props.meaning.definitions) {
+    return null;
+  }
+
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
 
       {props.meaning.definitions.map(function (definition, index) {
         return (
-          <div key={index}>
+          <div key={index} className="Definition">
             <p>{definition.definition}</p>
 
             {definition.example && (
-              <p className="Example">{definition.example}</p>
+              <div className="Example">{definition.example}</div>
             )}
           </div>
         );
       })}
+
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
